a231598ca2e34c1ef7a473ca3fc36840ad3c7d80
Priyanshu20048 <mittal.priyanshu2004@gmail.com>
2025-08-22 12:18:20 +0530

    Pttern 5

---
+++ Patterns/pattern5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+// 1
+// 2 2
+// 3 3 3 
+// 4 4 4 4
+// 5 5 5 5 5
+
+
+
+
+public class pattern5 {
+    public static void p4(int n){
+     for (int i =1;i<=n;i++){
+        for (int j =0;j<i;j++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+     }
+    }
+
+    public static void main (String [] args){
+    Scanner sc = new Scanner (System.in);
+    int n = sc.nextInt();
+    p4(n);
+}
+    
+}
